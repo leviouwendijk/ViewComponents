5ca6dcbfdd09b17b9c6510e444910d3d69fe27f3
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-05 17:10:05 +0200

    latest

--- Package.swift
+++ Package.swift
@@ -19,6 +19,10 @@ let package = Package(
             url: "https://github.com/leviouwendijk/plate.git",
             branch: "master"
         ),
+        .package(
+            url: "https://github.com/leviouwendijk/Structures.git",
+            branch: "master"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -26,7 +30,8 @@ let package = Package(
         .target(
             name: "ViewComponents",
             dependencies: [
-                .product(name: "plate", package: "plate")
+                .product(name: "plate", package: "plate"),
+                .product(name: "Structures", package: "Structures"),
             ],
             resources: [
                 .process("Resources")
@@ -36,7 +41,8 @@ let package = Package(
             name: "ViewComponentsTests",
             dependencies: [
                 "ViewComponents",
-                .product(name: "plate", package: "plate")
+                .product(name: "plate", package: "plate"),
+                .product(name: "Structures", package: "Structures"),
             ]
         ),
     ]
--- Sources/ViewComponents/views/fuzzy-search-field.swift
+++ Sources/ViewComponents/views/fuzzy-search-field.swift
@@ -1,22 +1,6 @@
 import SwiftUI
 import plate
-
-public enum SearchStrictness: Int, CaseIterable, Identifiable {
-    case exact = 0
-    case strict = 2
-    case loose  = 3
-
-    public var id: Self { self }
-    public var title: String {
-        switch self {
-        case .exact:  return "Exact"
-        case .strict: return "Strict"
-        case .loose:  return "Loose"
-        }
-    }
-
-    public var tolerance: Int { rawValue }
-}
+import Structures
 
 public struct FuzzySearchField: View {
     @Binding public var searchQuery: String
