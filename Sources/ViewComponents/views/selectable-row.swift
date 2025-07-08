931805c8a1063853ac2d1eb06c7c591c767c2e4b
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-08 12:42:11 +0200

    isDisabled

--- Sources/ViewComponents/views/selectable-row.swift
+++ Sources/ViewComponents/views/selectable-row.swift
@@ -5,17 +5,20 @@ import plate
 public struct SelectableRow: View {
     public let title: String
     public let isSelected: Bool
+    public let isDisabled: Bool
     public let action: () -> Void
     public let animationDuration: TimeInterval
 
     public init(
         title: String,
         isSelected: Bool,
+        isDisabled: Bool = false,
         animationDuration: Double = 0.2,
         action: @escaping () -> Void
     ) {
         self.title = title
         self.isSelected = isSelected
+        self.isDisabled = isDisabled
         self.animationDuration = animationDuration
         self.action = action
     }
@@ -28,6 +31,7 @@ public struct SelectableRow: View {
                     // .foregroundColor(Color("NearBlack"))
                     .foregroundColor(Color.primary)
                     // .bold()
+                    .strikethrough(isDisabled, color: .gray)
             } else {
                 Text(title)
                     // .font(.headline)
