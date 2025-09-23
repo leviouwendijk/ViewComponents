03eae2ffc808bb73992c2ef4e726333df9561084
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-09-23 16:52:19 +0200

    hiding strictness possible

--- Sources/ViewComponents/views/fuzzy-search-field.swift
+++ Sources/ViewComponents/views/fuzzy-search-field.swift
@@ -6,15 +6,18 @@ public struct FuzzySearchField: View {
     @Binding public var searchQuery: String
     @Binding public var searchStrictness: SearchStrictness
     public let title: String
+    public var hideStrictness: Bool
 
     public init(
         title: String? = nil,
         searchQuery: Binding<String>,
-        searchStrictness: Binding<SearchStrictness>
+        searchStrictness: Binding<SearchStrictness>,
+        hideStrictness: Bool = false
     ) {
         self._searchQuery = searchQuery
         self._searchStrictness = searchStrictness
         self.title = title ?? "Search"
+        self.hideStrictness = hideStrictness
     }
 
     public var body: some View {
@@ -22,23 +25,25 @@ public struct FuzzySearchField: View {
             TextField(title, text: $searchQuery)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
 
-            HStack(spacing: 4) {
-                ForEach(SearchStrictness.allCases) { level in
-                    Text(level.title)
-                    .font(.caption2)
-                    .padding(.vertical, 4)
-                    // .padding(.horizontal, 6)
-                    .padding(.horizontal, 6)
-                    .background(
-                        searchStrictness == level
-                            ? Color.accentColor.opacity(0.2)
-                            : Color.secondary.opacity(0.1)
-                    )
-                    .cornerRadius(4)
-                    .onTapGesture {
-                        searchStrictness = level
+            if !hideStrictness {
+                HStack(spacing: 4) {
+                    ForEach(SearchStrictness.allCases) { level in
+                        Text(level.title)
+                        .font(.caption2)
+                        .padding(.vertical, 4)
+                        // .padding(.horizontal, 6)
+                        .padding(.horizontal, 6)
+                        .background(
+                            searchStrictness == level
+                                ? Color.accentColor.opacity(0.2)
+                                : Color.secondary.opacity(0.1)
+                        )
+                        .cornerRadius(4)
+                        .onTapGesture {
+                            searchStrictness = level
+                        }
+                        .frame(minWidth: 50)
                     }
-                    .frame(minWidth: 50)
                 }
             }
         }
