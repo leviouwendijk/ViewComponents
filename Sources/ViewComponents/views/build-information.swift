af0a9a9b03066f91dd193de2625abc2f1ad68976
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-09-27 19:29:52 +0200

    commenting out buildinformation views, moving to compositions lib

--- Sources/ViewComponents/views/build-information.swift
+++ Sources/ViewComponents/views/build-information.swift
@@ -1,238 +1,222 @@
 import SwiftUI
 import plate
+import Structures
 
-public enum AlignmentStyle {
-    case leading
-    case trailing
-    case center
-}
+// @available(*, deprecated, message: "Superseded by BuildInformationSwitch")
+// public struct BuildInformationStatic: View {
+//     public let specification: BuildSpecification
+//     public let alignment: AlignmentStyle
+//     public let display: [BuildInformationDisplayComponents]
+//     public let prefixStyle: VersionPrefixStyle
 
-public enum BuildInformationDisplayComponents {
-    case version
-    case name
-    case author
-    case description
-    // case versionPrefix
-    case latestVersion
-}
+//     @State private var isUpdateAvailable: Bool = false
 
-// public enum VersionPrefixDisplayStyle {
-//     case short
-//     case long
+//     // remove hardcoding this
+//     public static let repoPkl = URL(string: "https://raw.githubusercontent.com/leviouwendijk/Responder/refs/heads/master/build-object.pkl")!
+
+//     public init(
+//         specification: BuildSpecification,
+//         alignment: AlignmentStyle = .center,
+//         display: [BuildInformationDisplayComponents] = [.version],
+//         prefixStyle: VersionPrefixStyle = .short
+//     ) {
+//         self.specification = specification
+//         self.alignment = alignment
+//         self.display = display
+//         self.prefixStyle = prefixStyle
+//     }
+
+//     // public var finalVersionString: String {
+//     //     var v = ""
+//     //     let prefix = prefixStyle == .short ? "v " : "version "
+//     //     if display.contains(.versionPrefix) { 
+//     //         v.append(prefix)
+//     //     }
+//     //     v.append(specification.versionString())
+//     //     return v
+//     // }
+    
+//     public var body: some View {
+//         HStack {
+//             if alignment == .trailing {
+//                 Spacer()
+//             }
+
+//             VStack {
+//                 if display.contains(.name) {
+//                     Text(specification.name)
+//                     .font(.footnote).foregroundColor(.secondary)
+//                 }
+                
+//                 if display.contains(.version) {
+//                     Text("deprecated")
+//                     .font(.footnote).foregroundColor(.secondary)
+//                 }
+
+//                 if display.contains(.author) {
+//                     Text(specification.author)
+//                     .font(.footnote).foregroundColor(.secondary)
+//                 }
+//             }
+
+//             if alignment == .leading {
+//                 Spacer()
+//             }
+//         }
+//         .padding(.horizontal)
+//         .padding(.vertical, 8)
+//         .background(Color(NSColor.windowBackgroundColor).opacity(0.1))
+//     }
 // }
 
-public struct BuildInformationStatic: View {
-    public let specification: BuildSpecification
-    public let alignment: AlignmentStyle
-    public let display: [BuildInformationDisplayComponents]
-    public let prefixStyle: VersionPrefixStyle
+// public struct BuildInformationSwitch: View {
+//     // public let specification: BuildSpecification
+//     // public let localBuild: BuildObjectConfiguration
+//     @StateObject public var viewmodel: BuildInformationViewModel
+//     public let alignment: AlignmentStyle
+//     public let display: [[BuildInformationDisplayComponents]]
+//     public let prefixStyle: VersionPrefixStyle
 
-    @State private var isUpdateAvailable: Bool = false
+//     @State public var current: Int = 0
 
-    // remove hardcoding this
-    public static let repoPkl = URL(string: "https://raw.githubusercontent.com/leviouwendijk/Responder/refs/heads/master/build-object.pkl")!
+//     // @State public var remoteBuild: BuildObjectConfiguration
 
-    public init(
-        specification: BuildSpecification,
-        alignment: AlignmentStyle = .center,
-        display: [BuildInformationDisplayComponents] = [.version],
-        prefixStyle: VersionPrefixStyle = .short
-    ) {
-        self.specification = specification
-        self.alignment = alignment
-        self.display = display
-        self.prefixStyle = prefixStyle
-    }
+//     @State private var isUpdateAvailable: Bool = false
+//     @State private var updateError: String = ""
 
-    // public var finalVersionString: String {
-    //     var v = ""
-    //     let prefix = prefixStyle == .short ? "v " : "version "
-    //     if display.contains(.versionPrefix) { 
-    //         v.append(prefix)
-    //     }
-    //     v.append(specification.versionString())
-    //     return v
-    // }
+//     public init(
+//         // specification: BuildSpecification = defaultBuildObject(),
+//         // localBuild: BuildObjectConfiguration = defaultBuildObject(),
+//         viewmodel: BuildInformationViewModel,
+//         alignment: AlignmentStyle = .center,
+//         display: [[BuildInformationDisplayComponents]] = [[.version], [.latestVersion], [.name], [.author]],
+//         prefixStyle: VersionPrefixStyle = .long
+//     ) {
+//         // self.specification = specification
+//         // self.localBuild = localBuild
+//         // self.remoteBuild = localBuild
+//         self.alignment = alignment
+//         self.display = display
+//         self.prefixStyle = prefixStyle
+//     }
     
-    public var body: some View {
-        HStack {
-            if alignment == .trailing {
-                Spacer()
-            }
+//     public var body: some View {
+//         Button(action: {
+//             withAnimation(.easeInOut) {
+//                 current = (current + 1) % display.count
+//             }
+//         }) {
+//             ZStack {
+//                 HStack {
+//                     if alignment == .trailing { Spacer() }
+//                     VStack {
+//                         if display[current].contains(.name) {
+//                             Text(localBuild.name)
+//                                 .font(.footnote)
+//                                 .foregroundColor(.secondary)
+//                         }
 
-            VStack {
-                if display.contains(.name) {
-                    Text(specification.name)
-                    .font(.footnote).foregroundColor(.secondary)
-                }
-                
-                if display.contains(.version) {
-                    Text("deprecated")
-                    .font(.footnote).foregroundColor(.secondary)
-                }
+//                         if display[current].contains(.version) {
+//                             VStack {
+//                                 if !(updateError.isEmpty) {
+//                                     NotificationBanner(
+//                                         type: .error,
+//                                         message: updateError
+//                                     )
+//                                 }
 
-                if display.contains(.author) {
-                    Text(specification.author)
-                    .font(.footnote).foregroundColor(.secondary)
-                }
-            }
+//                                 HStack {
+//                                     Text(localBuild.version.string(prefixStyle: prefixStyle))
+//                                         .font(.footnote)
+//                                         .foregroundColor(.secondary)
+//                                         .strikethrough(isUpdateAvailable)
 
-            if alignment == .leading {
-                Spacer()
-            }
-        }
-        .padding(.horizontal)
-        .padding(.vertical, 8)
-        .background(Color(NSColor.windowBackgroundColor).opacity(0.1))
-    }
-}
+//                                     if isUpdateAvailable {
+//                                         Text("update available (\(remoteBuild.version.string(prefixStyle: prefixStyle)))")
+//                                         .font(.footnote)
+//                                         // .fontWeight(.semibold)
+//                                         .foregroundColor(Color.orange)
+//                                     }
+//                                 }
+//                             }
+//                         }
 
-public struct BuildInformationSwitch: View {
-    // public let specification: BuildSpecification
-    public let localBuild: BuildObjectConfiguration
-    public let alignment: AlignmentStyle
-    public let display: [[BuildInformationDisplayComponents]]
-    public let prefixStyle: VersionPrefixStyle
+//                         if display[current].contains(.latestVersion) {
+//                             VStack {
+//                                 if !(updateError.isEmpty) {
+//                                     NotificationBanner(
+//                                         type: .error,
+//                                         message: updateError
+//                                     )
+//                                 }
 
-    @State public var current: Int = 0
+//                                 HStack {
+//                                     Text(remoteBuild.version.string(prefixStyle: prefixStyle, remote: true))
+//                                         .font(.footnote)
+//                                         .foregroundColor(.secondary)
 
-    @State public var remoteBuild: BuildObjectConfiguration
+//                                     if isUpdateAvailable {
+//                                         Text("update available")
+//                                         .font(.footnote)
+//                                         // .fontWeight(.semibold)
+//                                         .foregroundColor(Color.orange)
+//                                     }
+//                                 }
+//                             }
+//                         }
 
-    @State private var isUpdateAvailable: Bool = false
-    @State private var updateError: String = ""
+//                         if display[current].contains(.author) {
+//                             Text(localBuild.author)
+//                                 .font(.footnote)
+//                                 .foregroundColor(.secondary)
+//                         }
+//                     }
+//                     if alignment == .leading { Spacer() }
+//                 }
+//                 .id(current)
+//                 .transition(.asymmetric(
+//                     insertion: .move(edge: .bottom)
+//                                  .combined(with: .opacity),
+//                     removal:   .move(edge: .top)
+//                                  .combined(with: .opacity)
+//                 ))
+//             }
+//             .padding(.horizontal)
+//             .padding(.vertical, 8)
+//             .background(Color(NSColor.windowBackgroundColor).opacity(0.1))
+//         }
+//         .buttonStyle(.plain)
+//         .task {
+//             do {
+//                 let fetched = try await fetchRemoteBuildObject()
+//                 remoteBuild = fetched
+//                 isUpdateAvailable = fetched.version > localBuild.version
+//             } catch {
+//                 if let pklErr = error as? PklParserError {
+//                     updateError = pklErr.description
+//                 } else {
+//                     updateError = error.localizedDescription
+//                 }
+//             }
+//         }
+//     }
+// }
 
-    public init(
-        // specification: BuildSpecification = defaultBuildObject(),
-        localBuild: BuildObjectConfiguration = defaultBuildObject(),
-        alignment: AlignmentStyle = .center,
-        display: [[BuildInformationDisplayComponents]] = [[.version], [.latestVersion], [.name], [.author]],
-        prefixStyle: VersionPrefixStyle = .long
-    ) {
-        // self.specification = specification
-        self.localBuild = localBuild
-        self.remoteBuild = localBuild
-        self.alignment = alignment
-        self.display = display
-        self.prefixStyle = prefixStyle
-    }
-    
-    public var body: some View {
-        Button(action: {
-            withAnimation(.easeInOut) {
-                current = (current + 1) % display.count
-            }
-        }) {
-            ZStack {
-                HStack {
-                    if alignment == .trailing { Spacer() }
-                    VStack {
-                        if display[current].contains(.name) {
-                            Text(localBuild.name)
-                                .font(.footnote)
-                                .foregroundColor(.secondary)
-                        }
-
-                        if display[current].contains(.version) {
-                            VStack {
-                                if !(updateError.isEmpty) {
-                                    NotificationBanner(
-                                        type: .error,
-                                        message: updateError
-                                    )
-                                }
-
-                                HStack {
-                                    Text(localBuild.version.string(prefixStyle: prefixStyle))
-                                        .font(.footnote)
-                                        .foregroundColor(.secondary)
-                                        .strikethrough(isUpdateAvailable)
-
-                                    if isUpdateAvailable {
-                                        Text("update available (\(remoteBuild.version.string(prefixStyle: prefixStyle)))")
-                                        .font(.footnote)
-                                        // .fontWeight(.semibold)
-                                        .foregroundColor(Color.orange)
-                                    }
-                                }
-                            }
-                        }
-
-                        if display[current].contains(.latestVersion) {
-                            VStack {
-                                if !(updateError.isEmpty) {
-                                    NotificationBanner(
-                                        type: .error,
-                                        message: updateError
-                                    )
-                                }
-
-                                HStack {
-                                    Text(remoteBuild.version.string(prefixStyle: prefixStyle, remote: true))
-                                        .font(.footnote)
-                                        .foregroundColor(.secondary)
-
-                                    if isUpdateAvailable {
-                                        Text("update available")
-                                        .font(.footnote)
-                                        // .fontWeight(.semibold)
-                                        .foregroundColor(Color.orange)
-                                    }
-                                }
-                            }
-                        }
-
-                        if display[current].contains(.author) {
-                            Text(localBuild.author)
-                                .font(.footnote)
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                    if alignment == .leading { Spacer() }
-                }
-                .id(current)
-                .transition(.asymmetric(
-                    insertion: .move(edge: .bottom)
-                                 .combined(with: .opacity),
-                    removal:   .move(edge: .top)
-                                 .combined(with: .opacity)
-                ))
-            }
-            .padding(.horizontal)
-            .padding(.vertical, 8)
-            .background(Color(NSColor.windowBackgroundColor).opacity(0.1))
-        }
-        .buttonStyle(.plain)
-        .task {
-            do {
-                let fetched = try await fetchRemoteBuildObject()
-                remoteBuild = fetched
-                isUpdateAvailable = fetched.version > localBuild.version
-            } catch {
-                if let pklErr = error as? PklParserError {
-                    updateError = pklErr.description
-                } else {
-                    updateError = error.localizedDescription
-                }
-            }
-        }
-    }
-}
-
-public func defaultBuildObject() -> BuildObjectConfiguration {
-    do {
-        return try BuildObjectConfiguration()
-    } catch {
-        print("PKL parse failed in BuildInformationSwitch:", error)
-        return BuildObjectConfiguration(
-            name: "",
-            type: .application,
-            version: ObjectVersion(major: 0, minor: 0, patch: 0),
-            details: "",
-            author: "",
-            update: ""
-        )
-    }
-}
+// public func defaultBuildObject() -> BuildObjectConfiguration {
+//     do {
+//         return try BuildObjectConfiguration()
+//     } catch {
+//         print("PKL parse failed in BuildInformationSwitch:", error)
+//         return BuildObjectConfiguration(
+//             name: "",
+//             type: .application,
+//             version: ObjectVersion(major: 0, minor: 0, patch: 0),
+//             details: "",
+//             author: "",
+//             update: ""
+//         )
+//     }
+// }
 
 enum UpdateCheckError: Error, LocalizedError {
     case missingUpdateURL
